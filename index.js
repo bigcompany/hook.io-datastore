59564c0629afe5467fb1144ab7a16e126fc06329
Marak <marak.squires@gmail.com>
2016-03-11 01:03:13 -0500

    [api] Adds ability to auth

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ datastore.Datastore = function (opts) {
   opts.port = opts.port || 6379;
   opts.host = opts.host || "0.0.0.0";
   self.client = redis.createClient(opts.port, opts.host);
+  self.client.auth(opts.password);
   self.client.on("error", function (err) {
     console.log("Error " + err);
   });
